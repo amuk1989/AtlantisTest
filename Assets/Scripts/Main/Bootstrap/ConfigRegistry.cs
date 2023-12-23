25feab3bdd261acf5b3a5beeed2a3e33a6df02b7
Andrey Muholzoev <amuk89@mail.ru>
2023-12-23 20:44:10 +0700

    BaseScripts

---
+++ Assets/Scripts/Main/Bootstrap/ConfigRegistry.cs
@@ -0,0 +1,18 @@
+﻿using GameStage.Controllers;
+using UnityEngine;
+using Utility;
+using Zenject;
+
+namespace Main.Bootstrap
+{
+    [CreateAssetMenu(fileName = "ConfigRegistry", menuName = "Registries/ConfigRegistry", order = 0)]
+    public class ConfigRegistry : ScriptableObjectInstaller
+    {
+        [SerializeField] private GameStageConfig _gameStageConfig;
+            
+        public override void InstallBindings()
+        {
+            Container.InstallRegistry(_gameStageConfig.Data);
+        }
+    }
+}
---
+++ Assets/Scripts/Main/Bootstrap/MainInstaller.cs
@@ -0,0 +1,15 @@
+﻿using GameStage.Bootstrap;
+using Input.Bootstrap;
+using Zenject;
+
+namespace Main.Bootstrap
+{
+    public class MainInstaller: MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Install<GameStageInstaller>();
+            Container.Install<InputInstaller>();
+        }
+    }
+}
