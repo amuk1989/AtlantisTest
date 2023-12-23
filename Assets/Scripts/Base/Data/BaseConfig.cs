767fd92b42b0021189e9d1c62d3f5877097fbb65
Andrey Muholzoev <amuk89@mail.ru>
2023-12-23 18:38:25 +0700

    add plugins

---
+++ Assets/Scripts/Base/Data/BaseConfig.cs
@@ -0,0 +1,14 @@
+﻿using Base.Interfaces;
+using UnityEngine;
+using Utility;
+using Zenject;
+
+namespace Base.Data
+{
+    public abstract class BaseConfig<TData> : ScriptableObject where TData: IConfigData
+    {
+        [SerializeField] private TData _data;
+
+        public TData Data => _data;
+    }
+}
