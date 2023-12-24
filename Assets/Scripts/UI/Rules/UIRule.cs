c20599e69d59dead4892979f9fde77ba66548dee
Andrey Muholzoev <amuk89@mail.ru>
2023-12-24 23:01:27 +0700

    ui imported

---
+++ Assets/Scripts/UI/Rules/UIRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Base.Rules;
+using GameStage.Data;
+using GameStage.Interfaces;
+using UI.Views;
+using UniRx;
+using Zenject;
+
+namespace UI.Rules
+{
+    internal class UIRule: BaseGamesRule
+    {
+        private readonly CompositeDisposable _compositeDisposable = new();
+        
+        private readonly UIComponent _uiComponent;
+        private readonly BaseUI.Factory _uiPrefabFactory;
+        private BaseUI _changeAnimation;
+
+        public UIRule(IGameStageService gameStageService, 
+            UIComponent uiComponent, BaseUI.Factory uiPrefabFactory) : base(gameStageService)
+        {
+            _uiComponent = uiComponent;
+            _uiPrefabFactory = uiPrefabFactory;
+        }
+
+        protected override void OnStageChanged(GameStageId gameStageId)
+        {
+            
+        }
+    }
+}
